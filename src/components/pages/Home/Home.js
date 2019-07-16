2d74bbb4c7db51eae5ba70b8c3d7c19e805dd4b2
Hanna Söderström <gothbarbie84@gmail.com>
2019-07-16 13:10:05 +0200

    feat: Home page

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -2,16 +2,24 @@ import React from 'react'
 
 import Page from 'components/templates/Page'
 import Main from 'components/templates/Main'
+import Button from 'components/atoms/Button'
 
 import styles from './Home.module.scss'
 
 const Home = () => (
-  <Page data-testid="library-screen">
-    <Main>
-      <header>
-        <h1 className={styles.h1}>Home</h1>
-      </header>
-    </Main>
+  <Page data-testid="home-screen">
+    <div className={styles.wrapper}>
+      <Main>
+        <header className={styles.header}>
+          <h1 className={styles.h1}>Dinner Generator</h1>
+        </header>
+        <div className={styles.body}>
+          <Button>Generate Dinners</Button>
+          <Button>Create Dinner</Button>
+          <Button>What can I make with...?</Button>
+        </div>
+      </Main>
+    </div>
   </Page>
 )
 
