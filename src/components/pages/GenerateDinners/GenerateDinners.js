c560aed906cd0b141e9ec7d029cd937906ee5181
Hanna Söderström <gothbarbie84@gmail.com>
2019-07-18 15:11:26 +0200

    feat: add content

--- src/components/pages/GenerateDinners/GenerateDinners.js
+++ src/components/pages/GenerateDinners/GenerateDinners.js
@@ -3,29 +3,30 @@ import React from 'react'
 import Page from 'components/templates/Page'
 import Main from 'components/templates/Main'
 import Title from 'components/atoms/Title'
-import Navigation from 'components/atoms/Navigation'
+import DinnerThumbnail from 'components/atoms/DinnerThumbnail'
+import Ingredients from 'components/atoms/Ingredients'
+import Navigation from 'components/molecules/Navigation'
 
 // import styles from './GenerateDinners.module.scss'
+const ingredients = [
+  'Spaghetti, 1 normal cock-size',
+  'Grinded meat, 500g thats grams fuckhead',
+  'Mushroom, 2 cans',
+  'Cocktail tomatoes, 1/2 package',
+  'Cucumber, 1/3',
+]
 
 const GenerateDinners = () => (
   <Page data-testid="generatedinners-screen">
     <Main>
       <Title title="Generate Dinners" />
-      <Navigation />
+      <Navigation currentTab="generate-dinner" />
       <div>
-        <header>
-          <h2>Spaghetti and meat-sauce</h2>
-        </header>
-        <div>
-          <h3>Ingredients</h3>
-          <ul>
-            <li>Spaghetti, 1 normal cock-size</li>
-            <li>Grinded meat, 500g thats grams fuckhead</li>
-            <li>Mushroom, 2 cans</li>
-            <li>Cocktail tomatoes, 1/2 package</li>
-            <li>Cucumber, 1/3</li>
-          </ul>
-        </div>
+        <DinnerThumbnail
+          imgFileName={'spaghetti.jpg'}
+          name="Spaghetti and brain-sauce"
+        />
+        <Ingredients ingredients={ingredients} />
       </div>
     </Main>
   </Page>
