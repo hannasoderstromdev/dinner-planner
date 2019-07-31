beb652b68837b3dafd74fb3d8f512e7bf4694cc1
Hanna Söderström <gothbarbie84@gmail.com>
2019-07-31 14:48:45 +0200

    feat: add WeekdayDinner and Groceries

--- src/components/pages/GenerateDinners/GenerateDinners.js
+++ src/components/pages/GenerateDinners/GenerateDinners.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Page from 'components/templates/Page'
 import Main from 'components/templates/Main'
 import Title from 'components/atoms/Title'
 import DinnerThumbnail from 'components/atoms/DinnerThumbnail'
 import Ingredients from 'components/atoms/Ingredients'
+import WeekdayDinner from 'components/atoms/WeekdayDinner'
+import Groceries from 'components/atoms/Groceries'
 import Navigation from 'components/molecules/Navigation'
 
-// import styles from './GenerateDinners.module.scss'
+import styles from './GenerateDinners.module.scss'
+
 const ingredients = [
   'Spaghetti, 1 normal cock-size',
   'Grinded meat, 500g thats grams fuckhead',
@@ -16,20 +19,109 @@ const ingredients = [
   'Cucumber, 1/3',
 ]
 
-const GenerateDinners = () => (
-  <Page data-testid="generatedinners-screen">
-    <Main>
-      <Title title="Generate Dinners" />
-      <Navigation currentTab="generate-dinner" />
-      <div>
-        <DinnerThumbnail
-          imgFileName={'spaghetti.jpg'}
-          name="Spaghetti and brain-sauce"
-        />
-        <Ingredients ingredients={ingredients} />
-      </div>
-    </Main>
-  </Page>
-)
+const groceries = [
+  'Spaghetti, 1 normal cock-size',
+  'Grinded meat, 500g thats grams fuckhead',
+  'Mushroom, 2 cans',
+  'Cocktail tomatoes, 1/2 package',
+  'Cucumber, 1/3',
+]
+
+const weekdaysWithDinners = [
+  {
+    name: 'Macaronies n bacon',
+    link: 'macaronies-n-bacon',
+    weekday: 'Monday',
+  },
+  {
+    name: 'Pancakes',
+    link: 'pancakes',
+    weekday: 'Tuesday',
+  },
+  {
+    name: 'Fish sticks',
+    link: 'fish-sticks',
+    weekday: 'Wednesday',
+  },
+  {
+    name: 'Heavenly stew',
+    link: 'heavenly-stew',
+    weekday: 'Thursday',
+  },
+  {
+    name: 'Spaghetti and meat-sauce',
+    link: 'spaghetti-and-meat-sauce',
+    weekday: 'Friday',
+  },
+  {
+    name: 'Tacos',
+    link: 'tacos',
+    weekday: 'Saturday',
+  },
+  {
+    name: 'Sausage-stroganoff',
+    link: 'sausage-stroganoff',
+    weekday: 'Sunday',
+  },
+]
+
+const GenerateDinners = () => {
+  const [currentTab, setCurrentTab] = useState('generate-dinner')
+  const [includedWeekdays, setIncludedWeekdays] = useState({
+    monday: false,
+    tuesday: false,
+    wednesday: false,
+    thursday: false,
+    friday: false,
+    saturday: false,
+    sunday: false,
+  })
+
+  const toggleWeekdays = (weekday, value) => {
+    setIncludedWeekdays({
+      ...includedWeekdays,
+      [weekday]: value,
+    })
+  }
+
+  return (
+    <Page data-testid="generatedinners-screen">
+      <Main>
+        <Title title="Generate Dinners" />
+        <Navigation currentTab={currentTab} setCurrentTab={setCurrentTab} />
+
+        {currentTab === 'generate-dinner' ? (
+          <div>
+            <DinnerThumbnail
+              imgFileName={'spaghetti.jpg'}
+              name="Spaghetti and brain-sauce"
+            />
+            <Ingredients ingredients={ingredients} />
+          </div>
+        ) : (
+          <React.Fragment>
+            <div className={styles.dinnerList}>
+              {weekdaysWithDinners.map(weekday => (
+                <WeekdayDinner
+                  checked={includedWeekdays[weekday.weekday.toLowerCase()]}
+                  dinner={{
+                    name: weekday.name,
+                    link: weekday.link,
+                  }}
+                  key={weekday.weekday}
+                  onCheckChange={toggleWeekdays}
+                  weekday={weekday.weekday}
+                />
+              ))}
+            </div>
+            <div className={styles.groceriesList}>
+              <Groceries groceries={groceries} />
+            </div>
+          </React.Fragment>
+        )}
+      </Main>
+    </Page>
+  )
+}
 
 export default GenerateDinners
