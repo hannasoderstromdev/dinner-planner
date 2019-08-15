e318c550393200ad8e5654e91cb3e18f446e9540
Hanna Söderström <gothbarbie84@gmail.com>
2019-08-15 13:16:57 +0200

    feat: remove iconsize

--- src/components/atoms/Icon/Icon.js
+++ src/components/atoms/Icon/Icon.js
@@ -1,21 +1,12 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const Icon = ({
-  className,
-  color,
-  testId,
-  icon,
-  iconsize,
-  onMouseOver,
-  onMouseOut,
-}) => (
+const Icon = ({ className, color, testId, icon, onMouseOver, onMouseOut }) => (
   <FontAwesomeIcon
     className={className}
     color={color}
     data-testid={testId ? testId : 'icon'}
     icon={icon}
-    iconsize={iconsize}
     onBlur={onMouseOut}
     onFocus={onMouseOver}
     onMouseOut={onMouseOut}
@@ -26,7 +17,6 @@ const Icon = ({
 Icon.defaultProps = {
   className: '',
   color: '',
-  iconsize: '1rem',
   onMouseOut: null,
   onMouseOver: null,
   testId: undefined,
